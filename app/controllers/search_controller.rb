d19bee2a6feadd60a7c3d32e49b6ce701610e771
Tatsuya-15 <fiuto54gkar@outlook.com>
2021-06-17 14:44:33 +0000

    検索機能追加

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,36 @@
+class SearchController < ApplicationController
+  def search
+    @model = params["search"]["model"]  #選択したmodelを@modelに代入
+    @value = params["search"]["value"]  #検索にかけた文字列(ここではvalue)を@valueに代入
+    @how = params["search"]["how"]      #選択した検索方法howを@howに代入
+    @datas = search_for(@how, @model, @value)      #search_forの引数にインスタンス変数を定義
+  end
+  
+  private
+
+  def match(model, value)                     #def search_forでhowがmatchだった場合の処理
+    if model == 'member'                        #modelがuserの場合の処理
+      Member.where(last_name: value)                 #whereでvalueと完全一致するnameを探します
+    elsif model == 'customer'
+      Customer.where(company: value)
+    end
+  end
+
+  def partical(model, value)
+    if model == 'member'                       
+      Member.where(last_name: value)                
+    elsif model == 'customer'
+      Customer.where(company: value)
+    end
+  end
+
+  def search_for(how, model, value)       #searchアクションで定義した情報が引数に入っている
+    case how                              #検索方法のhowの中身がどれなのかwhenの条件分岐の中から探す処理
+    when 'match'
+      match(model, value)                 #検索方法の引数に(model, value)を定義している
+    when 'partical'
+      partical(model, value)
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'search/search'
   devise_for :members
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
   resources :customers, only: [:index, :show, :edit, :new, :update, :destroy, :create]
   resources :members, only: [:index, :show, :edit, :update]
+  get '/search' => 'search#search'
 end
